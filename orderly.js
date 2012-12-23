f59aceab0b3cf40d0c21245af9fc42d8e7e41e29
Peter Minarik <peter@peterminarik.com>
2012-12-23 15:13:09 +0100

    Define methods on prototype with extend

--- orderly.js
+++ orderly.js
@@ -1,11 +1,11 @@
 (function() {
   var Orderly,
       global,
+      supports,
       defaultOptions = {
         itemSelector: 'li',
         handleSelector: ''
-      },
-      supports;
+      };
 
   supports = (function () {
     return {
@@ -18,6 +18,156 @@
       })()
     };
   })();
+
+  Orderly = function (el, options) {
+    this.settings = $.extend({}, defaultOptions, options);
+    this.$list = $(el).first();
+
+    this.onDragStart = this.onDragStart.bind(this, supports.touch);
+    this.onDragMove  = this.onDragMove.bind(this, supports.touch);
+    this.onDragEnd   = this.onDragEnd.bind(this, supports.touch);
+  };
+
+  $.extend(Orderly.prototype, {
+
+    settings: null,
+    height: 0,
+
+    $list: null,
+    $items: null,
+
+    enable: function() {
+      toggleStartListeners.call(this, true);
+
+      this.$items = this.getItems();
+      this.itemCount = this.getItemCount();
+      this.height = this.getItemHeight();
+    },
+
+    disable: function() {
+      toggleStartListeners.call(this, false);
+
+      this.$items = null;
+      this.itemCount = 0;
+      this.height = 0;
+    },
+
+    onDragStart: function(isTouch, event) {
+      var coordObj = isTouch ? event.originalEvent.changedTouches[0] : event;
+
+      $(isTouch ? event.target : document)
+        .on(isTouch ? 'touchmove' : 'mousemove', this.onDragMove)
+        .on(isTouch ? 'touchend' : 'mouseup', this.onDragEnd);
+
+      this.startX = coordObj.pageX;
+      this.startY = coordObj.pageY;
+      this.start();
+      return false;
+    },
+
+    onDragMove: function(isTouch, event) {
+      var coordObj = isTouch ? event.originalEvent.changedTouches[0] : event;
+      this.move(coordObj.pageX - this.startX, coordObj.pageY - this.startY);
+    },
+
+    onDragEnd: function(isTouch, event) {
+      $(isTouch ? event.target : document)
+        .off(isTouch ? 'touchmove' : 'mousemove', this.onDragMove)
+        .off(isTouch ? 'touchend' : 'mouseup', this.onDragEnd);
+
+      delete this.startX;
+      delete this.startY;
+      this.end();
+    },
+
+    getItemCount: function() {
+      return (this.$items && this.$items.length) ? this.$items.length : 0;
+    },
+
+    getItemHeight: function() {
+      if (this.$items.length > 1) {
+        return this.$items.get(1).offsetTop - this.$items.get(0).offsetTop;
+      } else {
+        return 0;
+      }
+    },
+
+    getItems: function() {
+      return this.$list.find(this.settings.itemSelector);
+    },
+
+    getDraggingElement: function() {
+      if (this.settings.handleSelector) {
+        return $(event.target).parent(this.settings.itemSelector);
+      } else {
+        return $(event.target);
+      }
+    },
+
+    start: function() {
+      this.$items = this.getItems();
+      this.$dragging = this.getDraggingElement();
+
+      toggleSortingStyles.call(this, true);
+
+      this.startIndex = this.$items.index(this.$dragging);
+      this.currentIndex = this.startIndex;
+    },
+
+    move: function(dx, dy) {
+      var indexDiff, newIndex;
+
+      // Move dragged object
+      this.$dragging.css({
+        top: dy,
+        left: dx
+      });
+
+      indexDiff = Math.round(dy / this.height);
+      indexDiff = Math.max(indexDiff, -this.startIndex); 
+      indexDiff = Math.min(indexDiff, this.itemCount - this.startIndex - 1);
+      newIndex = this.startIndex + indexDiff;
+
+      // If the current index did not change return
+      if (this.currentIndex === newIndex) {
+        return;
+      }
+
+      this.currentIndex = newIndex;
+
+      this.$items.each(function(i, item) {
+        var top = '';
+
+        // If this item is the dragged one we don't need to do anything
+        if (i === this.startIndex) {
+          return;
+        }
+
+        if (i < this.startIndex && i >= newIndex) {
+          top = this.height;
+        } else if (i > this.startIndex && i <= newIndex) {
+          top = -this.height;
+        }
+
+        $(item).css('top', top);
+      }.bind(this));
+    },
+
+    end: function() {
+      var insertFunc = this.currentIndex < this.startIndex ? 'insertBefore' : 'insertAfter',
+          insertEl = this.$items.eq(this.currentIndex);
+
+      toggleSortingStyles.call(this, false);
+      
+      if (this.currentIndex !== this.startIndex) {
+        this.$dragging[insertFunc](insertEl);
+      };
+
+      delete this.currentIndex;
+      delete this.startIndex;
+      delete this.$dragging;
+    }
+  });
 
   function toggleSortingStyles (toggle) {
     if (toggle) {
@@ -42,145 +192,10 @@
     this.$list.toggleClass('orderly-sorting-list', toggle);
   }
 
-  Orderly = function (el, options) {
-    this.settings = $.extend({}, defaultOptions, options);
-    this.$list = $(el).first();
-    this.$items = null;
-    this.height = 0;
-    this._enabled = false;
-  };
-
-  Orderly.prototype.enable = function() {
-    this.onDragStart = this.onDragStart.bind(this, supports.touch);
-    this.onDragMove  = this.onDragMove.bind(this, supports.touch);
-    this.onDragEnd   = this.onDragEnd.bind(this, supports.touch);
-    this.$list.on(supports.touch ? 'touchstart' : 'mousedown', [this.settings.itemSelector, this.settings.handleSelector].join(' '), this.onDragStart);
-
-    this.setItems();
-    this.setItemHeight();
-    this._enabled = true;
-  };
-
-  Orderly.prototype.disable = function() {
-    this.$list.off(supports.touch ? 'touchstart' : 'mousedown', [this.settings.itemSelector, this.settings.handleSelector].join(' '), this.onDragStart);
-
-    this.$items = null;
-    this.height = 0;
-    this._enabled = false;
-  };
-
-  Orderly.prototype.onDragStart = function(isTouch, event) {
-    var coordObj = isTouch ? event.originalEvent.changedTouches[0] : event;
-
-    $(isTouch ? event.target : document)
-      .on(isTouch ? 'touchmove' : 'mousemove', this.onDragMove)
-      .on(isTouch ? 'touchend' : 'mouseup', this.onDragEnd);
-
-    this.startX = coordObj.pageX;
-    this.startY = coordObj.pageY;
-    this.start();
-    return false;
-  };
-
-  Orderly.prototype.onDragMove = function(isTouch, event) {
-    var coordObj = isTouch ? event.originalEvent.changedTouches[0] : event;
-    this.move(coordObj.pageX - this.startX, coordObj.pageY - this.startY);
-  };
-
-  Orderly.prototype.onDragEnd = function(isTouch, event) {
-    $(isTouch ? event.target : document)
-      .off(isTouch ? 'touchmove' : 'mousemove', this.onDragMove)
-      .off(isTouch ? 'touchend' : 'mouseup', this.onDragEnd);
-
-    delete this.startX;
-    delete this.startY;
-    this.end();
-  };
-
-  Orderly.prototype.setItemHeight = function() {
-    if (this.$items.length > 1) {
-      return this.height = this.$items.get(1).offsetTop - this.$items.get(0).offsetTop;
-    } else {
-      return this.height = 0;
-    }
-  };
-
-  Orderly.prototype.setItems = function() {
-    this.$items = this.$list.find(this.settings.itemSelector);
-    this.itemCount = this.$items.length;
-  };
-
-  Orderly.prototype.setDragging = function() {
-    if (this.settings.handleSelector) {
-      this.$dragging = $(event.target).parent(this.settings.itemSelector);
-    } else {
-      this.$dragging = $(event.target);
-    }
-  };
-
-  Orderly.prototype.start = function() {
-    this.setItems();
-    this.setDragging();
-
-    toggleSortingStyles.call(this, true);
-
-    this.startIndex = this.$items.index(this.$dragging);
-    this.currentIndex = this.startIndex;
-  };
-
-  Orderly.prototype.move = function(dx, dy) {
-    var indexDiff, newIndex;
-
-    // Move dragged object
-    this.$dragging.css({
-      top: dy,
-      left: dx
-    });
-
-    indexDiff = Math.round(dy / this.height);
-    indexDiff = Math.max(indexDiff, -this.startIndex); 
-    indexDiff = Math.min(indexDiff, this.itemCount - this.startIndex - 1);
-    newIndex = this.startIndex + indexDiff;
-
-    // If the current index did not change return
-    if (this.currentIndex === newIndex) {
-      return;
-    }
-
-    this.currentIndex = newIndex;
-
-    this.$items.each(function(i, item) {
-      var top = '';
-
-      // If this item is the dragged one we don't need to do anything
-      if (i === this.startIndex) {
-        return;
-      }
-
-      if (i < this.startIndex && i >= newIndex) {
-        top = this.height;
-      } else if (i > this.startIndex && i <= newIndex) {
-        top = -this.height;
-      }
-
-      $(item).css('top', top);
-    }.bind(this));
-  };
-
-  Orderly.prototype.end = function() {
-    var insertFunc = this.currentIndex < this.startIndex ? 'insertBefore' : 'insertAfter',
-        insertEl = this.$items.eq(this.currentIndex);
-
-    toggleSortingStyles.call(this, false);
-    
-    if (this.currentIndex !== this.startIndex) {
-      this.$dragging[insertFunc](insertEl);
-    };
-
-    delete this.currentIndex;
-    delete this.startIndex;
-    delete this.$dragging;
-  };
+  function toggleStartListeners (toggle) {
+    var onOff = toggle ? 'on' : 'off';
+    this.$list[onOff](supports.touch ? 'touchstart' : 'mousedown', [this.settings.itemSelector, this.settings.handleSelector].join(' '), this.onDragStart);
+  }
 
   if (typeof module !== 'undefined' && module.exports) {
     module.exports = Orderly;
